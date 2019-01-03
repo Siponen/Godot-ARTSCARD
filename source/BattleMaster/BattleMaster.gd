fc543bb73863f6ee4763ad1e6838138d83bd42c3
Conny Siponen <c.siponen@gmail.com>
2019-01-03 22:45:27 +0100

    Add Battlemaster

--- source/BattleMaster/BattleMaster.gd
+++ source/BattleMaster/BattleMaster.gd
@@ -1,11 +1,49 @@
 extends Spatial
 
-var foo
+#The server battlemaster will RPC the involved players
+#And the involved players will RPC back to the server
+
+#The game will have Action points
+#Which you spend on building structures
+
+var playerOne
+var playerTwo
+
+const NUM_ACTION_POINTS = 4
+
+var turn = 1
+
+var phase = 1
+
 
 func _ready():
+	playerOne = $PlayerOne
+	playerTwo = $PlayerTwo
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	pass
+
+func tick():
+	phase += 1
+	
+	match(phase):
+		1: pass
+	
+	pass
+
+func refresh(player):
+	#Refresh action points
+	player.actionPoints = NUM_ACTION_POINTS
+	
+	#for effect in player.refreshEffects:
+	#	pass
+	
+	#Refresh cards
+	#for card in player.activeCards:
+	#	pass
+	#pass
+	
+master func drawCard(player):
+	var card = player.drawCard()
+	pass
---
+++ source/CardPlayer/CardPlayer.gd
@@ -0,0 +1,20 @@
+extends Spatial
+
+var hand = []
+var deck = []
+var graveyard = []
+#var banished = []
+
+func _ready():
+	pass
+
+master func loadDeck():
+	pass
+
+master func drawCard(player):
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
