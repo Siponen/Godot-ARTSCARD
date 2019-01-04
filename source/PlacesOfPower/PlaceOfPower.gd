1c4dfe5529a3c6aaecfb7cd529b873a613eae07f
Conny Siponen <c.siponen@gmail.com>
2019-01-04 03:13:29 +0100

    Add board nodes and begin on mapping out all adjacent tiles.

---
+++ source/PlacesOfPower/PlaceOfPower.gd
@@ -0,0 +1,11 @@
+extends StaticBody
+
+export(Array, NodePath) var AdjacentNodes
+
+func _ready():
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
